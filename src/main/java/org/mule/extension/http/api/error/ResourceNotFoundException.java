d9e5d2c233440f03d5f965e29c24c0642c75ca17
afelisatti <afelisatti@mulesoft.com>
2017-05-15 18:19:54 -0300

    MULE-12485: Remove listener interpretRequestErrors feature. Only specific errors with specific attributes will be considered.

--- src/main/java/org/mule/extension/http/api/error/ResourceNotFoundException.java
+++ src/main/java/org/mule/extension/http/api/error/ResourceNotFoundException.java
@@ -6,16 +6,21 @@
  */
 package org.mule.extension.http.api.error;
 
+import static org.mule.service.http.api.HttpConstants.HttpStatus.NOT_FOUND;
+import org.mule.extension.http.api.HttpListenerResponseAttributes;
+import org.mule.runtime.api.exception.ErrorMessageAwareException;
 import org.mule.runtime.api.i18n.I18nMessage;
+import org.mule.runtime.api.message.Message;
 import org.mule.runtime.extension.api.error.ErrorTypeDefinition;
 import org.mule.runtime.extension.api.exception.ModuleException;
+import org.mule.service.http.api.domain.ParameterMap;
 
 /**
  * Thrown when a static file is requested but not found, associated with a 404 status code.
  *
  * @since 1.0
  */
-public class ResourceNotFoundException extends ModuleException {
+public class ResourceNotFoundException extends ModuleException implements ErrorMessageAwareException {
 
   private static final long serialVersionUID = 3137973689262542839L;
 
@@ -25,4 +30,17 @@ public class ResourceNotFoundException extends ModuleException {
     super(exception, errorTypeDefinition, message);
   }
 
+  @Override
+  public Message getErrorMessage() {
+    return Message.builder()
+        .payload(null)
+        .attributes(new HttpListenerResponseAttributes(NOT_FOUND.getStatusCode(), NOT_FOUND.getReasonPhrase(),
+                                                       new ParameterMap()))
+        .build();
+  }
+
+  @Override
+  public Throwable getRootCause() {
+    return this;
+  }
 }
