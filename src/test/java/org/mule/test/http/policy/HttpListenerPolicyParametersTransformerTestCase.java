1358f93858589d3148e8ee2639d76e7a3399c6ee
Federico Amdan <famdan@mulesoft.com>
2017-09-13 17:14:42 -0300

    PR review

--- src/test/java/org/mule/test/http/policy/HttpListenerPolicyParametersTransformerTestCase.java
+++ src/test/java/org/mule/test/http/policy/HttpListenerPolicyParametersTransformerTestCase.java
@@ -6,8 +6,11 @@
  */
 package org.mule.test.http.policy;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.mule.test.http.AllureConstants.HttpFeature.HTTP_EXTENSION;
+import static org.mule.test.http.AllureConstants.HttpFeature.HttpStory.ERROR_HANDLING;
 
 import org.mule.extension.http.api.HttpRequestAttributes;
 import org.mule.extension.http.api.listener.builder.HttpListenerErrorResponseBuilder;
@@ -16,28 +19,51 @@ import org.mule.runtime.api.message.Message;
 import org.mule.runtime.api.metadata.MediaType;
 import org.mule.runtime.api.util.MultiMap;
 
+import java.security.cert.Certificate;
 import java.util.Map;
 
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import org.junit.Test;
 
+@Feature(HTTP_EXTENSION)
+@Story(ERROR_HANDLING)
 public class HttpListenerPolicyParametersTransformerTestCase {
+
+  private final String EXPECTED_PAYLOAD = "{'message': 'this is the payload'}";
+  private final MediaType EXPECTED_MEDIA_TYPE = MediaType.APPLICATION_JSON;
+
+  //HttpRequestAttributes parameters
+  private final MultiMap<String, String> HEADERS = new MultiMap<>();
+  private final String LISTENER_PATH = "/test";
+  private final String RELATIVE_PATH = "/";
+  private final String VERSION = "1";
+  private final String SCHEME = "scheme";
+  private final String METHOD = "GET";
+  private final String REQUEST_PATH = "/test";
+  private final String REQUEST_URI = "";
+  private final String QUERY_STRING = "";
+  private final MultiMap<String, String> QUERY_PARAMS = new MultiMap<>();
+  private final MultiMap<String, String> URI_PARAMS = new MultiMap<>();
+  private final String REMOTE_ADDRESS = "";
+  private final Certificate CLIENT_CERTIFICATE = null;
 
   @Test
   public void getErrorResponseBodyWhenSendingHttpRequestAttributes() {
     HttpListenerPolicyParametersTransformer transformer = new HttpListenerPolicyParametersTransformer();
-    Message.Builder messageBuilder = (Message.Builder) Message.builder();
-    HttpRequestAttributes httpRequestAttributes =
-        new HttpRequestAttributes(new MultiMap<String, String>(), "/test", "/", "1", "scheme", "GET", "/test", "", "",
-                                  new MultiMap<String, String>(), new MultiMap<String, String>(), "", null);
-    messageBuilder.attributesValue(httpRequestAttributes);
-    String expectedPayload = "{'message': 'this is the payload'}";
-    messageBuilder.value(expectedPayload);
-    messageBuilder.mediaType(MediaType.APPLICATION_JSON);
-    Map<String, Object> result = transformer.fromMessageToErrorResponseParameters(messageBuilder.build());
+    Message message = Message.builder().value(EXPECTED_PAYLOAD)
+        .mediaType(EXPECTED_MEDIA_TYPE)
+        .attributesValue(new HttpRequestAttributes(HEADERS, LISTENER_PATH, RELATIVE_PATH, VERSION, SCHEME, METHOD, REQUEST_PATH,
+                                                   REQUEST_URI, QUERY_STRING, QUERY_PARAMS, URI_PARAMS, REMOTE_ADDRESS,
+                                                   CLIENT_CERTIFICATE))
+        .build();
 
-    assertNotNull(result);
-    assertEquals(expectedPayload, ((HttpListenerErrorResponseBuilder) result.get("errorResponse")).getBody().getValue());
-    assertEquals("application/json", ((HttpListenerErrorResponseBuilder) result.get("errorResponse")).getBody().getDataType()
-        .getMediaType().toString());
+    Map<String, Object> result = transformer.fromMessageToErrorResponseParameters(message);
+
+    assertThat(result, is(notNullValue()));
+    assertThat(((HttpListenerErrorResponseBuilder) result.get("errorResponse")).getBody().getValue().toString(),
+               is(EXPECTED_PAYLOAD));
+    assertThat(((HttpListenerErrorResponseBuilder) result.get("errorResponse")).getBody().getDataType().getMediaType().toString(),
+               is(EXPECTED_MEDIA_TYPE.toString()));
   }
 }
