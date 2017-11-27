060c74bbaad7d9240bd5e47ed9a9f83749cfabdc
Alejandro Pulver <alejandro.pulver@mulesoft.com>
2017-11-27 14:16:07 -0300

    MULE-14082: add CRL feature to Mule 4

---
+++ src/test/java/org/mule/test/http/functional/requester/HttpStandardRevocationConfigTestCase.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test.http.functional.requester;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assume.assumeTrue;
+import static org.mule.functional.junit4.matchers.MessageMatchers.hasPayload;
+import static org.mule.runtime.http.api.HttpConstants.HttpStatus.OK;
+
+import org.mule.extension.http.api.HttpResponseAttributes;
+import org.mule.functional.junit4.MuleArtifactFunctionalTestCase;
+import org.mule.runtime.core.api.event.CoreEvent;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.hamcrest.MatcherAssert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class HttpStandardRevocationConfigTestCase extends MuleArtifactFunctionalTestCase {
+
+  @Override
+  protected String getConfigFile() {
+    return "http-requester-standard-revocation-config.xml";
+  }
+
+  @Before
+  public void setup() {
+    Boolean reachable = false;
+    try {
+      URL url = new URL("http://www.google.com/");
+      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+      conn.connect();
+      if (conn.getResponseCode() == 200) {
+        reachable = true;
+        conn.disconnect();
+      }
+    } catch (Exception e) {
+      // ignore, already false by default
+    }
+
+    assumeTrue("Check for internet connection, and access to www.google.com", reachable);
+  }
+
+  @Test
+  public void revocationWorksWithoutTrustStore() throws Exception {
+    CoreEvent event = flowRunner("test").keepStreamsOpen().run();
+    MatcherAssert.assertThat(((HttpResponseAttributes) event.getMessage().getAttributes().getValue()).getStatusCode(), is(OK.getStatusCode()));
+    assertThat(event.getMessage(), hasPayload(containsString("webcache.googleusercontent.com")));
+  }
+}
---
+++ src/test/java/org/mule/test/http/functional/requester/ocsp/AbstractHttpOcspRevocationTestCase.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.test.http.functional.requester.ocsp;
+
+import static java.lang.String.format;
+import static org.junit.Assume.assumeFalse;
+
+import org.mule.tck.junit4.rule.DynamicPort;
+import org.mule.tck.junit4.rule.SystemProperty;
+import org.mule.test.http.functional.AbstractHttpTlsRevocationTestCase;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+
+public abstract class AbstractHttpOcspRevocationTestCase extends AbstractHttpTlsRevocationTestCase {
+
+  @Rule
+  public DynamicPort dynamicOcspPort;
+
+  @Rule
+  public SystemProperty certAliasSystemProperty;
+
+  private static String RUN_OCSP_SERVER_COMMAND = "openssl ocsp " +
+      "-index src/test/resources/tls/ocsp/%s " +
+      "-port %d " +
+      "-CA src/test/resources/tls/ocsp/server.crt " +
+      "-rkey src/test/resources/tls/ocsp/%s.pem " +
+      "-rsigner src/test/resources/tls/ocsp/%s.crt";
+
+  static final String REVOKED_OCSP_LIST = "revoked-ocsp.txt";
+
+  static final String VALID_OCSP_LIST = "valid-ocsp.txt";
+
+  /**
+   *  This certified entity was generated to test revocation with OCSP mechanism.
+   */
+  protected static final String ENTITY_CERTIFIED_REVOCATION_OCSP_SUB_PATH = "entityOcsp1";
+
+  /**
+   * The entity4 certificate has a hardcoded ocsp url: http://localhost:1111
+   */
+  static final int FIXED_OCSP_PORT = 1111;
+
+
+  static final String DEFAULT_OCSP_RESPONDER = "server";
+
+  private Process process;
+
+  private final String ocspList;
+
+  private int ocspPort;
+
+  private String ocspResponder;
+
+
+  AbstractHttpOcspRevocationTestCase(String configFile, String ocspList, String certAlias) {
+    super(configFile, ENTITY_CERTIFIED_REVOCATION_OCSP_SUB_PATH);
+    this.ocspList = ocspList;
+    initialiseOcspStatus(certAlias);
+  }
+
+  private void initialiseOcspStatus(String certAlias) {
+    if (certAlias != null) {
+      certAliasSystemProperty = new SystemProperty("certAlias", certAlias);
+      dynamicOcspPort = new DynamicPort("dynamicOcspPort");
+      ocspPort = dynamicOcspPort.getNumber();
+      ocspResponder = certAliasSystemProperty.getValue();
+    } else {
+      ocspPort = FIXED_OCSP_PORT;
+      ocspResponder = DEFAULT_OCSP_RESPONDER;
+    }
+  }
+
+
+  @Before
+  public void setUp() throws Exception {
+    process = Runtime.getRuntime().exec(format(RUN_OCSP_SERVER_COMMAND, ocspList, ocspPort, ocspResponder, ocspResponder));
+    assumeFalse("Since openssl ocsp command has a flaky behavior the test will be ignored if an error occurs in server initialisation.",
+                getOcspServerCommandOutput(process.getErrorStream()).contains("Error"));
+  }
+
+  @After
+  public void tearDown() throws IOException {
+    if (process != null) {
+      process.destroy();
+    }
+  }
+
+  /**
+   * @param commandOutput the error stream of the ocsp server process.
+   * @return the first line of the ocsp server command output.
+   * @throws IOException
+   */
+  private String getOcspServerCommandOutput(InputStream commandOutput) throws IOException {
+    char aux;
+    StringBuilder stringBuilder = new StringBuilder();
+    while ((aux = (char) commandOutput.read()) != '\n') {
+      stringBuilder.append(aux);
+    }
+    return stringBuilder.toString();
+  }
+
+}
