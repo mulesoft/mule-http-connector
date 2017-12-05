93f7e592f714a850ddd0608ff1f9b1bb3312163c
Alejandro García Marra <alemarra@gmail.com>
2017-12-05 17:12:41 -0300

    MULE-14165: Add default headers and queryParameters to HTTP Requester config (#155)

---
+++ src/main/java/org/mule/extension/http/api/request/builder/RequestHeader.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.extension.http.api.request.builder;
+
+import org.mule.runtime.extension.api.annotation.Alias;
+import org.mule.runtime.extension.api.annotation.param.Parameter;
+
+/**
+ * Represents an HTTP Header
+ *
+ * @since 1.1
+ */
+@Alias("defaultHeader")
+public class RequestHeader {
+
+  @Parameter
+  private String key;
+
+  @Parameter
+  private String value;
+
+  public String getKey() {
+    return key;
+  }
+
+  public String getValue() {
+    return value;
+  }
+
+}
