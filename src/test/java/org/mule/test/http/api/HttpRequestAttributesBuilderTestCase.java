0c3010fc46a718469d20ade8c34830bda691e305
Ana Felisatti <afelisatti@mulesoft.com>
2018-03-19 09:26:27 -0300

    MULE-14457: HttpRequestAttributesBuilder does not validate entries (#212)

---
+++ src/test/java/org/mule/test/http/api/HttpRequestAttributesBuilderTestCase.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test.http.api;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+import static org.mule.functional.junit4.matchers.ThrowableMessageMatcher.hasMessage;
+import static org.mule.test.http.AllureConstants.HttpFeature.HTTP_EXTENSION;
+import org.mule.extension.http.api.HttpRequestAttributesBuilder;
+import org.mule.runtime.api.util.MultiMap;
+import org.mule.tck.junit4.AbstractMuleTestCase;
+
+import io.qameta.allure.Feature;
+import org.junit.Test;
+
+@Feature(HTTP_EXTENSION)
+public class HttpRequestAttributesBuilderTestCase extends AbstractMuleTestCase {
+
+  private HttpRequestAttributesBuilder builder = new HttpRequestAttributesBuilder();
+
+  @Test
+  public void headers() {
+    assertFailure(() -> builder.headers(null), "HTTP headers cannot be null.");
+  }
+
+  @Test
+  public void queryParams() {
+    assertFailure(() -> builder.queryParams(null), "Query params cannot be null.");
+  }
+
+  @Test
+  public void uriParams() {
+    assertFailure(() -> builder.uriParams(null), "URI params cannot be null.");
+  }
+
+  @Test
+  public void queryString() {
+    assertFailure(() -> builder.queryString(null), "Query string cannot be null.");
+  }
+
+  @Test
+  public void listenerPath() {
+    assertFailure(() -> builder.build(), "Listener path cannot be null.");
+  }
+
+  @Test
+  public void relativePath() {
+    assertFailure(() -> builder
+        .listenerPath("api/v2/*")
+        .build(), "Relative path cannot be null.");
+  }
+
+  @Test
+  public void version() {
+    assertFailure(() -> builder
+        .listenerPath("api/v2/*")
+        .relativePath("clients")
+        .build(), "HTTP version cannot be null.");
+  }
+
+  @Test
+  public void scheme() {
+    assertFailure(() -> builder
+        .listenerPath("api/v2/*")
+        .relativePath("clients")
+        .version("1.1")
+        .build(), "Scheme cannot be null.");
+  }
+
+  @Test
+  public void method() {
+    assertFailure(() -> builder
+        .listenerPath("api/v2/*")
+        .relativePath("clients")
+        .version("1.1")
+        .scheme("https")
+        .build(), "HTTP method cannot be null.");
+  }
+
+  @Test
+  public void requestPath() {
+    assertFailure(() -> builder
+        .listenerPath("api/v2/*")
+        .relativePath("clients")
+        .version("1.1")
+        .scheme("https")
+        .method("GET")
+        .build(), "Request path cannot be null.");
+  }
+
+  @Test
+  public void requestUri() {
+    assertFailure(() -> builder
+        .listenerPath("api/v2/*")
+        .relativePath("clients")
+        .version("1.1")
+        .scheme("https")
+        .method("GET")
+        .requestPath("/api/v2/clients")
+        .build(), "Request URI cannot be null.");
+  }
+
+  @Test
+  public void localAddress() {
+    MultiMap<String, String> params = new MultiMap<>();
+    params.put("from", "ITA");
+
+    assertFailure(() -> builder
+        .listenerPath("api/v2/*")
+        .relativePath("clients")
+        .version("1.1")
+        .scheme("https")
+        .method("GET")
+        .requestPath("/api/v2/clients")
+        .queryString("from=ITA")
+        .queryParams(params)
+        .requestUri("api/v2/clients?from=ITA")
+        .build(), "Local address cannot be null.");
+  }
+
+  @Test
+  public void remoteAddress() {
+    MultiMap<String, String> params = new MultiMap<>();
+    params.put("from", "ITA");
+
+    assertFailure(() -> builder
+        .listenerPath("api/v2/*")
+        .relativePath("clients")
+        .version("1.1")
+        .scheme("https")
+        .method("GET")
+        .requestPath("/api/v2/clients")
+        .queryString("from=ITA")
+        .queryParams(params)
+        .requestUri("api/v2/clients?from=ITA")
+        .localAddress("localhost/127.0.0.1:8081")
+        .build(), "Remote address cannot be null.");
+  }
+
+  private void assertFailure(Runnable closure, String message) {
+    try {
+      closure.run();
+      fail("Expected NullPointerException.");
+    } catch (Exception e) {
+      assertThat(e, is(instanceOf(NullPointerException.class)));
+      assertThat(e, hasMessage(equalTo(message)));
+    }
+  }
+
+}
--- src/test/java/org/mule/test/http/api/HttpRequestAttributesTestCase.java
+++ src/test/java/org/mule/test/http/api/HttpRequestAttributesTestCase.java
@@ -8,13 +8,11 @@ package org.mule.test.http.api;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-
 import org.mule.extension.http.api.HttpRequestAttributesBuilder;
 import org.mule.runtime.api.util.MultiMap;
 import org.mule.tck.junit4.AbstractMuleTestCase;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.HashMap;
 
 import org.junit.Test;
 
@@ -26,7 +24,7 @@ public class HttpRequestAttributesTestCase extends AbstractMuleTestCase {
       "   Method=GET\n" +
       "   Listener path=/listener/path\n" +
       "   Local Address=http://127.0.0.1:8080/\n" +
-      "   Query String=queryString\n" +
+      "   Query String=queryParam1=queryParam1&queryParam2=queryParam2\n" +
       "   Relative Path=/relative/path\n" +
       "   Remote Address=http://10.1.2.5:8080/\n" +
       "   Request Uri=http://127.0.0.1/gateway\n" +
@@ -48,16 +46,16 @@ public class HttpRequestAttributesTestCase extends AbstractMuleTestCase {
 
   private static final String TO_STRING_EMPTY = "org.mule.extension.http.api.HttpRequestAttributes\n" +
       "{\n" +
-      "   Request path=null\n" +
-      "   Method=null\n" +
-      "   Listener path=null\n" +
-      "   Local Address=null\n" +
-      "   Query String=null\n" +
-      "   Relative Path=null\n" +
-      "   Remote Address=null\n" +
-      "   Request Uri=null\n" +
-      "   Scheme=null\n" +
-      "   Version=null\n" +
+      "   Request path=/request/path\n" +
+      "   Method=GET\n" +
+      "   Listener path=/listener/path\n" +
+      "   Local Address=http://127.0.0.1:8080/\n" +
+      "   Query String=\n" +
+      "   Relative Path=/relative/path\n" +
+      "   Remote Address=http://10.1.2.5:8080/\n" +
+      "   Request Uri=http://127.0.0.1/gateway\n" +
+      "   Scheme=scheme\n" +
+      "   Version=1.0\n" +
       "   Headers=[]\n" +
       "   Query Parameters=[]\n" +
       "   URI Parameters=[]\n" +
@@ -65,16 +63,16 @@ public class HttpRequestAttributesTestCase extends AbstractMuleTestCase {
 
   private static final String TO_STRING_QUERY_PARAMS = "org.mule.extension.http.api.HttpRequestAttributes\n" +
       "{\n" +
-      "   Request path=null\n" +
-      "   Method=null\n" +
-      "   Listener path=null\n" +
-      "   Local Address=null\n" +
-      "   Query String=null\n" +
-      "   Relative Path=null\n" +
-      "   Remote Address=null\n" +
-      "   Request Uri=null\n" +
-      "   Scheme=null\n" +
-      "   Version=null\n" +
+      "   Request path=/request/path\n" +
+      "   Method=GET\n" +
+      "   Listener path=/listener/path\n" +
+      "   Local Address=http://127.0.0.1:8080/\n" +
+      "   Query String=queryParam1=queryParam1&queryParam2=queryParam2\n" +
+      "   Relative Path=/relative/path\n" +
+      "   Remote Address=http://10.1.2.5:8080/\n" +
+      "   Request Uri=http://127.0.0.1/gateway\n" +
+      "   Scheme=scheme\n" +
+      "   Version=1.0\n" +
       "   Headers=[]\n" +
       "   Query Parameters=[\n" +
       "      queryParam1=queryParam1\n" +
@@ -85,16 +83,16 @@ public class HttpRequestAttributesTestCase extends AbstractMuleTestCase {
 
   private static final String TO_STRING_URI_PARAMS = "org.mule.extension.http.api.HttpRequestAttributes\n" +
       "{\n" +
-      "   Request path=null\n" +
-      "   Method=null\n" +
-      "   Listener path=null\n" +
-      "   Local Address=null\n" +
-      "   Query String=null\n" +
-      "   Relative Path=null\n" +
-      "   Remote Address=null\n" +
-      "   Request Uri=null\n" +
-      "   Scheme=null\n" +
-      "   Version=null\n" +
+      "   Request path=/request/path\n" +
+      "   Method=GET\n" +
+      "   Listener path=/listener/path\n" +
+      "   Local Address=http://127.0.0.1:8080/\n" +
+      "   Query String=\n" +
+      "   Relative Path=/relative/path\n" +
+      "   Remote Address=http://10.1.2.5:8080/\n" +
+      "   Request Uri=http://127.0.0.1/gateway\n" +
+      "   Scheme=scheme\n" +
+      "   Version=1.0\n" +
       "   Headers=[]\n" +
       "   Query Parameters=[]\n" +
       "   URI Parameters=[\n" +
@@ -103,35 +101,48 @@ public class HttpRequestAttributesTestCase extends AbstractMuleTestCase {
       "   ]\n" +
       "}";
 
+  private HttpRequestAttributesBuilder baseBuilder = new HttpRequestAttributesBuilder()
+      .listenerPath("/listener/path")
+      .relativePath("/relative/path")
+      .version("1.0")
+      .scheme("scheme")
+      .method("GET")
+      .requestPath("/request/path")
+      .remoteAddress("http://10.1.2.5:8080/")
+      .localAddress("http://127.0.0.1:8080/")
+      .requestUri("http://127.0.0.1/gateway");
+
   private Object requestAttributes;
 
   @Test
   public void completeToString() throws IllegalAccessException, InvocationTargetException, InstantiationException {
-    HttpRequestAttributesBuilder builder = new HttpRequestAttributesBuilder();
-    requestAttributes = builder.headers(getHeaders()).listenerPath("/listener/path").relativePath("/relative/path")
-        .version("1.0").scheme("scheme").method("GET").requestPath("/request/path").remoteAddress("http://10.1.2.5:8080/")
-        .queryString("queryString").queryParams(getQueryParams()).uriParams(getUriParams())
-        .localAddress("http://127.0.0.1:8080/").requestUri("http://127.0.0.1/gateway").build();
+    requestAttributes = baseBuilder
+        .headers(getHeaders())
+        .queryString("queryParam1=queryParam1&queryParam2=queryParam2")
+        .queryParams(getQueryParams())
+        .uriParams(getUriParams())
+        .build();
 
     assertThat(TO_STRING_COMPLETE, is(requestAttributes.toString()));
   }
 
   @Test
-  public void emptyToString() throws IllegalAccessException, InvocationTargetException, InstantiationException {
-    assertThat(TO_STRING_EMPTY, is(new HttpRequestAttributesBuilder().headers(new MultiMap<>())
-        .queryParams(new MultiMap<>()).uriParams(new HashMap<>()).build().toString()));
+  public void defaultToString() throws IllegalAccessException, InvocationTargetException, InstantiationException {
+    assertThat(TO_STRING_EMPTY, is(baseBuilder.build().toString()));
   }
 
   @Test
   public void onlyQueryParamToString() throws IllegalAccessException, InvocationTargetException, InstantiationException {
-    assertThat(TO_STRING_QUERY_PARAMS, is(new HttpRequestAttributesBuilder()
-        .headers(new MultiMap<>()).queryParams(getQueryParams()).uriParams(new HashMap<>()).build().toString()));
+    assertThat(TO_STRING_QUERY_PARAMS, is(baseBuilder
+        .queryParams(getQueryParams())
+        .queryString("queryParam1=queryParam1&queryParam2=queryParam2")
+        .build()
+        .toString()));
   }
 
   @Test
   public void onlyUriParamToString() throws IllegalAccessException, InvocationTargetException, InstantiationException {
-    assertThat(TO_STRING_URI_PARAMS, is(new HttpRequestAttributesBuilder()
-        .headers(new MultiMap<>()).uriParams(getUriParams()).queryParams(new MultiMap<>()).build().toString()));
+    assertThat(TO_STRING_URI_PARAMS, is(baseBuilder.uriParams(getUriParams()).build().toString()));
   }
 
   private MultiMap<String, String> getHeaders() {
