bd7107e4a783c2bf8ad8c84daf98388f229ec326
Rodrigo Merino <elrodro83@users.noreply.github.com>
2018-09-27 16:09:44 -0300

    MULE-15756: HTTP Requester removes payload when retrying (#265)

---
+++ src/main/java/org/mule/extension/http/internal/request/OffsetCursorProviderWrapper.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.extension.http.internal.request;
+
+import org.mule.runtime.api.exception.MuleRuntimeException;
+import org.mule.runtime.api.streaming.CursorProvider;
+import org.mule.runtime.api.streaming.bytes.CursorStream;
+import org.mule.runtime.api.streaming.bytes.CursorStreamProvider;
+
+import java.io.IOException;
+
+/**
+ * Wrapper for a {@link CursorProvider} that returns cursors with some offset.
+ */
+final class OffsetCursorProviderWrapper implements CursorStreamProvider {
+
+  private final long offset;
+  private final CursorStreamProvider provider;
+
+  public OffsetCursorProviderWrapper(CursorStreamProvider provider, long offset) {
+    this.offset = offset;
+    this.provider = provider;
+  }
+
+  @Override
+  public CursorStream openCursor() {
+    CursorStream cursor = provider.openCursor();
+    try {
+      cursor.seek(offset);
+    } catch (IOException e) {
+      throw new MuleRuntimeException(e);
+    }
+    return cursor;
+  }
+
+  @Override
+  public void close() {
+    provider.close();
+  }
+
+  @Override
+  public void releaseResources() {
+    provider.releaseResources();
+  }
+
+  @Override
+  public boolean isClosed() {
+    return provider.isClosed();
+  }
+}
