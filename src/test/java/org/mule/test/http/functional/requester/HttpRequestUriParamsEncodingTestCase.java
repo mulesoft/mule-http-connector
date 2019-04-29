906831cc075b7d47c14bd240ca868ffbfe3be7a5
Ana Felisatti <afelisatti@mulesoft.com>
2019-04-29 06:49:04 -0300

    MULE-16772: Allow the encoding of URI parameters in the requester (#322)

---
+++ src/test/java/org/mule/test/http/functional/requester/HttpRequestUriParamsEncodingTestCase.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test.http.functional.requester;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.mule.extension.http.api.HttpMessageBuilder.refreshSystemProperties;
+import static org.mule.extension.http.internal.HttpConnectorConstants.ENCODE_URI_PARAMS_PROPERTY;
+import org.mule.tck.junit4.rule.SystemProperty;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+
+public class HttpRequestUriParamsEncodingTestCase extends HttpRequestUriParamsTestCase {
+
+  @Rule
+  public SystemProperty uriParamsEncoding = new SystemProperty(ENCODE_URI_PARAMS_PROPERTY, "true");
+
+  @Before
+  public void setUp() {
+    refreshSystemProperties();
+  }
+
+  @After
+  public void tearDown() {
+    refreshSystemProperties();
+  }
+
+  @Override
+  public void uriParamsContainsReservedUriCharacter() throws Exception {
+    flowRunner("reservedUriCharacter").withPayload(TEST_MESSAGE)
+        .withVariable("paramName", "testParam").withVariable("paramValue", "$a/word%here\\").run();
+
+    assertThat(uri, equalTo("/testPath/%24a%2Fword%25here%5C"));
+  }
+}
