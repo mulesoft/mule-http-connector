4917340f88c855255ebb753838787366142c2bf3
Rodrigo Merino <elrodro83@users.noreply.github.com>
2019-12-20 11:02:28 -0300

    MULE-17848: Add builder methods for http attribute classes (#377) (#378)
    * Also, add support for MTF tests

--- src/main/java/org/mule/extension/http/internal/request/builder/HttpResponseAttributesBuilder.java
+++ src/main/java/org/mule/extension/http/internal/request/builder/HttpResponseAttributesBuilder.java
@@ -6,7 +6,11 @@
  */
 package org.mule.extension.http.internal.request.builder;
 
+import static java.util.Objects.requireNonNull;
+import static org.mule.runtime.api.util.MultiMap.emptyMultiMap;
+
 import org.mule.extension.http.api.HttpResponseAttributes;
+import org.mule.runtime.api.util.MultiMap;
 import org.mule.runtime.http.api.domain.message.response.HttpResponse;
 
 /**
@@ -14,17 +18,35 @@ import org.mule.runtime.http.api.domain.message.response.HttpResponse;
  */
 public class HttpResponseAttributesBuilder {
 
-  HttpResponse response;
+  private MultiMap<String, String> headers = emptyMultiMap();
+  private int statusCode;
+  private String reasonPhrase;
 
   public HttpResponseAttributesBuilder setResponse(HttpResponse response) {
-    this.response = response;
+    this.headers = response.getHeaders();
+    this.statusCode = response.getStatusCode();
+    this.reasonPhrase = response.getReasonPhrase();
+
+    return this;
+  }
+
+  public HttpResponseAttributesBuilder headers(MultiMap<String, String> headers) {
+    requireNonNull(headers, "HTTP headers cannot be null.");
+    this.headers = headers;
+    return this;
+  }
+
+  public HttpResponseAttributesBuilder statusCode(int statusCode) {
+    this.statusCode = statusCode;
+    return this;
+  }
+
+  public HttpResponseAttributesBuilder reasonPhrase(String reasonPhrase) {
+    this.reasonPhrase = reasonPhrase;
     return this;
   }
 
   public HttpResponseAttributes build() {
-    int statusCode = response.getStatusCode();
-    String reasonPhrase = response.getReasonPhrase();
-
-    return new HttpResponseAttributes(statusCode, reasonPhrase, response.getHeaders());
+    return new HttpResponseAttributes(statusCode, reasonPhrase, headers);
   }
 }
