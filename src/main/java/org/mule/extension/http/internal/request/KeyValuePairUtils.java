5a3cf1d23892eb4eff38efd86d63729203b8d799
Martín Buchwald <martin.buchwald@mulesoft.com>
2021-12-02 12:34:28 -0300

    HTTPC-171: Create HttpRequesterSimpleRequestBuilder for HttpPollingSource and Connectivity Testing (#562)

---
+++ src/main/java/org/mule/extension/http/internal/request/KeyValuePairUtils.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.extension.http.internal.request;
+
+import org.mule.extension.http.api.request.builder.KeyValuePair;
+import org.mule.runtime.api.util.MultiMap;
+
+
+public final class KeyValuePairUtils {
+
+  private KeyValuePairUtils() {
+    // Empty private constructor to avoid instantiation.
+  }
+
+  /**
+   * Returns a MultiMap representation of an iterable (e.g. a List) of KeyValuePairs
+   * @param iterable
+   * @return
+   */
+  public static MultiMap<String, String> toMultiMap(Iterable<? extends KeyValuePair> iterable) {
+    MultiMap<String, String> asMultiMap = new MultiMap<>();
+    for (KeyValuePair pair : iterable) {
+      asMultiMap.put(pair.getKey(), pair.getValue());
+    }
+    return asMultiMap;
+  }
+}
