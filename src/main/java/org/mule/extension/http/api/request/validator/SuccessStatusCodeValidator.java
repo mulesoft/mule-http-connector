9eac09a3a4f681765a2096e81b3b1ffd4a644a4e
Ezequiel Werner <eze210@gmail.com>
2021-12-13 13:14:36 -0300

    HTTPC-187: Expression based response validator (#576)

--- src/main/java/org/mule/extension/http/api/request/validator/SuccessStatusCodeValidator.java
+++ src/main/java/org/mule/extension/http/api/request/validator/SuccessStatusCodeValidator.java
@@ -8,6 +8,7 @@ package org.mule.extension.http.api.request.validator;
 
 
 import org.mule.extension.http.api.HttpResponseAttributes;
+import org.mule.runtime.extension.api.annotation.dsl.xml.TypeDsl;
 import org.mule.runtime.extension.api.runtime.operation.Result;
 import org.mule.runtime.extension.api.runtime.streaming.StreamingHelper;
 import org.mule.runtime.http.api.domain.message.request.HttpRequest;
@@ -21,6 +22,7 @@ import java.util.function.IntConsumer;
  *
  * @since 1.0
  */
+@TypeDsl(allowTopLevelDefinition = true)
 public class SuccessStatusCodeValidator extends RangeStatusCodeValidator {
 
   public SuccessStatusCodeValidator() {}
