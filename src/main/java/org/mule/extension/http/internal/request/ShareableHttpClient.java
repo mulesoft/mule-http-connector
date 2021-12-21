1ce5f831e9232e6bba780ac1a00ded383d75842b
Art Contreras <Art.Contreras@mulesoft.com>
2021-12-20 16:13:26 -0800

    [CSCA-1272] made ShareableHttpClient a top level class

---
+++ src/main/java/org/mule/extension/http/internal/request/ShareableHttpClient.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.extension.http.internal.request;
+
+import org.mule.runtime.http.api.client.HttpClient;
+import org.mule.runtime.http.api.client.auth.HttpAuthentication;
+import org.mule.runtime.http.api.domain.message.request.HttpRequest;
+import org.mule.runtime.http.api.domain.message.response.HttpResponse;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * Wrapper implementation of an {@link HttpClient} that allows being shared by only configuring the client when first required
+ * and only disabling it when last required.
+ */
+public class ShareableHttpClient {
+
+  private HttpClient delegate;
+  private Integer usageCount = new Integer(0);
+
+  ShareableHttpClient(HttpClient client) {
+    delegate = client;
+  }
+
+  public synchronized void start() {
+    if (++usageCount == 1) {
+      try {
+        delegate.start();
+      } catch (Exception e) {
+        usageCount--;
+        throw e;
+      }
+    }
+  }
+
+  public synchronized void stop() {
+    // In case this fails we do not want the usageCount to be reincremented
+    // as it will not be further used. If shouldn't be the case that more than
+    // two stops happen.
+    if (--usageCount == 0) {
+      delegate.stop();
+    }
+  }
+
+  public CompletableFuture<HttpResponse> sendAsync(HttpRequest request, int responseTimeout, boolean followRedirects,
+                                                   HttpAuthentication authentication) {
+    return delegate.sendAsync(request, responseTimeout, followRedirects, authentication);
+  }
+}
