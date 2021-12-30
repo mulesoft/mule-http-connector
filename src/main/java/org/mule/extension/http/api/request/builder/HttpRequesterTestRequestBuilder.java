f8d7bfbf879d380be572aa088a81a397fd8c0344
Ezequiel Werner <eze210@gmail.com>
2021-12-30 12:42:49 -0300

    HTTPC-192: Make connectivity testing body to accept a literal expression (#587)

--- src/main/java/org/mule/extension/http/api/request/builder/HttpRequesterTestRequestBuilder.java
+++ src/main/java/org/mule/extension/http/api/request/builder/HttpRequesterTestRequestBuilder.java
@@ -11,17 +11,24 @@ import static java.util.Collections.emptyList;
 import static java.util.Collections.unmodifiableList;
 import static java.util.Objects.hash;
 import static org.mule.runtime.api.meta.ExpressionSupport.NOT_SUPPORTED;
+import static org.mule.runtime.api.meta.ExpressionSupport.SUPPORTED;
+import static org.mule.runtime.api.metadata.DataType.STRING;
 
+import org.mule.runtime.api.lifecycle.Initialisable;
+import org.mule.runtime.api.lifecycle.InitialisationException;
+import org.mule.runtime.core.api.el.ExpressionManager;
 import org.mule.runtime.extension.api.annotation.Expression;
 import org.mule.runtime.extension.api.annotation.param.NullSafe;
 import org.mule.runtime.extension.api.annotation.param.Optional;
 import org.mule.runtime.extension.api.annotation.param.Parameter;
 import org.mule.runtime.extension.api.annotation.param.display.DisplayName;
 import org.mule.runtime.extension.api.annotation.param.display.Text;
+import org.mule.runtime.extension.api.runtime.parameter.Literal;
 import org.mule.runtime.http.api.domain.entity.EmptyHttpEntity;
 import org.mule.runtime.http.api.domain.entity.HttpEntity;
 import org.mule.runtime.http.api.domain.entity.InputStreamHttpEntity;
 
+import javax.inject.Inject;
 import java.io.ByteArrayInputStream;
 import java.util.List;
 import java.util.Objects;
@@ -31,18 +38,17 @@ import java.util.Objects;
  *
  * @since 1.7
  */
-public class HttpRequesterTestRequestBuilder {
+public class HttpRequesterTestRequestBuilder implements Initialisable {
 
   /**
-   * The body in the connectivity test request. It can't be an expression because it doesn't make sense in a
-   * connectivity testing context.
+   * The body in the connectivity test request. It can be an expression, but it won't have any binding.
    */
   @Parameter
-  @Optional(defaultValue = "")
+  @Optional
   @Text
-  @Expression(value = NOT_SUPPORTED)
+  @Expression(value = SUPPORTED)
   @DisplayName("Body")
-  private String requestBody;
+  private Literal<String> requestBody;
 
   /**
    * HTTP headers the connectivity test request should include. It allows multiple headers with the same key.
@@ -73,6 +79,12 @@ public class HttpRequesterTestRequestBuilder {
   @Expression(value = NOT_SUPPORTED)
   @DisplayName("URI Parameters")
   private List<UriParam> requestUriParams = emptyList();
+
+  @Inject
+  private ExpressionManager expressionManager;
+
+  // The request body can be an expression, but as it hasn't any binding we can cache the result in this variable.
+  private String resolvedRequestBody;
 
   public List<TestRequestHeader> getRequestHeaders() {
     return unmodifiableList(requestHeaders);
@@ -105,10 +117,42 @@ public class HttpRequesterTestRequestBuilder {
   }
 
   public HttpEntity buildEntity() {
-    if (requestBody == null || requestBody.isEmpty()) {
+    if (resolvedRequestBody == null || resolvedRequestBody.isEmpty()) {
       return new EmptyHttpEntity();
     } else {
-      return new InputStreamHttpEntity(new ByteArrayInputStream(requestBody.getBytes(UTF_8)));
+      return new InputStreamHttpEntity(new ByteArrayInputStream(resolvedRequestBody.getBytes(UTF_8)));
     }
+  }
+
+  @Override
+  public void initialise() throws InitialisationException {
+    this.resolvedRequestBody = resolveRequestBody();
+  }
+
+  private String resolveRequestBody() {
+    if (requestBody == null) {
+      // The request body isn't present.
+      return null;
+    }
+
+    java.util.Optional<String> optRequestBody = requestBody.getLiteralValue();
+    if (!optRequestBody.isPresent()) {
+      // The request body isn't present.
+      return null;
+    }
+
+    String requestBodyValue = optRequestBody.get();
+    if (isExpression(requestBodyValue)) {
+      // Resolve the expression.
+      return (String) expressionManager.evaluate(requestBodyValue, STRING).getValue();
+    } else {
+      // Just return the Literal value.
+      return requestBodyValue;
+    }
+  }
+
+  private static boolean isExpression(String value) {
+    String trim = value.trim();
+    return trim.startsWith("#[") && trim.endsWith("]");
   }
 }
