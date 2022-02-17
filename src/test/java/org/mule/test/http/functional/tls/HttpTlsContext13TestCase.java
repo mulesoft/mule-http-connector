86e5c1da51e3b9a607b272bacab30cc658f30e0e
pabloperalta <pablojulianperalta@gmail.com>
2022-02-17 10:43:33 -0300

    W-10667956: Validate and document TLSv1.3 usage

---
+++ src/test/java/org/mule/test/http/functional/tls/HttpTlsContext13TestCase.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test.http.functional.tls;
+
+import static org.mule.functional.junit4.matchers.MessageMatchers.hasPayload;
+
+import static java.lang.Integer.parseInt;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.mule.functional.api.exception.ExpectedError;
+import org.mule.tck.junit4.rule.DynamicPort;
+import org.mule.tck.junit4.rule.SystemProperty;
+
+import org.apache.commons.lang3.JavaVersion;
+import org.apache.commons.lang3.SystemUtils;
+import org.junit.Assume;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class HttpTlsContext13TestCase extends AbstractHttpTlsContextTestCase {
+
+  private static final String client = "tls-1.3-client";
+  private static final String SUCCESS_RESPONSE = "success";
+
+  @ClassRule
+  public static DynamicPort httpsInternalPort1 = new DynamicPort("internal.port.1");
+
+  @ClassRule
+  public static DynamicPort httpsInternalPort2 = new DynamicPort("internal.port.2");
+
+  @ClassRule
+  public static SystemProperty clientProtocols = new SystemProperty("jdk.tls.client.protocols", "TLSv1.3");
+
+  @ClassRule
+  public static SystemProperty muleSecurityModel = new SystemProperty("mule.security.model", "custom");
+
+  @ClassRule
+  public static SystemProperty verboseExceptions = new SystemProperty("mule.verbose.exceptions", "true");
+
+  @Rule
+  public ExpectedError expectedError = ExpectedError.none();
+
+  @Override
+  protected String getConfigFile() {
+    return "http-tls-1.3-test.xml";
+  }
+
+  @BeforeClass
+  public static void assumeJdkIsCompatible() {
+    String[] javaVersion = System.getProperty("java.version").split("_");
+    Assume.assumeTrue(SystemUtils.isJavaVersionAtLeast(JavaVersion.JAVA_1_8)
+        && (!javaVersion[0].equals("1.8.0") || parseInt(javaVersion[1]) > 261));
+  }
+
+  @Test
+  public void testBothProtocolsOneCipher() throws Exception {
+    assertThat(flowRunner(client).keepStreamsOpen().run().getMessage(), hasPayload(equalTo(SUCCESS_RESPONSE)));
+  }
+}
