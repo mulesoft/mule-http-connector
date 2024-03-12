fb355cb0ff544eaad64e77cce5bd87cbffaae4b6
Ezequiel Werner <eze210@gmail.com>
2024-03-12 14:36:06 -0300

    restore tests

--- functional-tests/http/src/test/java/org/mule/test/http/functional/listener/HttpListenerPersistentConnections10TestCase.java
+++ functional-tests/http/src/test/java/org/mule/test/http/functional/listener/HttpListenerPersistentConnections10TestCase.java
@@ -6,9 +6,7 @@
  */
 package org.mule.test.http.functional.listener;
 
-import static org.hamcrest.Matchers.anyOf;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
 import static org.mule.runtime.http.api.HttpHeaders.Values.CLOSE;
 import static org.mule.runtime.http.api.HttpHeaders.Values.KEEP_ALIVE;
@@ -16,7 +14,6 @@ import static org.mule.runtime.http.api.HttpHeaders.Values.KEEP_ALIVE;
 import java.io.IOException;
 
 import org.apache.http.HttpVersion;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class HttpListenerPersistentConnections10TestCase extends HttpListenerPersistentConnectionsTestCase {
@@ -28,8 +25,7 @@ public class HttpListenerPersistentConnections10TestCase extends HttpListenerPer
 
   @Test
   public void nonPersistentCheckHeader() throws Exception {
-    // TODO: Netty doesn't put the "close" when it's the default for the given http version. Review if we want this behavior
-    assertThat(performRequest(nonPersistentPort.getNumber(), getHttpVersion(), false), anyOf(is(CLOSE), is(nullValue())));
+    assertThat(performRequest(nonPersistentPort.getNumber(), getHttpVersion(), false), is(CLOSE));
   }
 
   @Test
@@ -41,14 +37,12 @@ public class HttpListenerPersistentConnections10TestCase extends HttpListenerPer
 
   @Test
   public void persistentCloseHeaderCheckHeader() throws Exception {
-    // TODO: Netty doesn't put the "close" when it's the default for the given http version. Review if we want this behavior
-    assertThat(performRequest(persistentPortCloseHeader.getNumber(), getHttpVersion(), false), anyOf(is(CLOSE), is(nullValue())));
+    assertThat(performRequest(persistentPortCloseHeader.getNumber(), getHttpVersion(), false), is(CLOSE));
   }
 
   @Test
-  @Ignore("TODO: Re-enable this test, it's failing for Netty only. See MULE-8502")
   public void persistentEchoCheckHeader() throws IOException {
-    assertThat(performRequest(persistentStreamingPort.getNumber(), getHttpVersion(), true), anyOf(is(CLOSE), is(nullValue())));
+    assertThat(performRequest(persistentStreamingPort.getNumber(), getHttpVersion(), true), is(CLOSE));
   }
 
   /**
@@ -62,7 +56,6 @@ public class HttpListenerPersistentConnections10TestCase extends HttpListenerPer
    * @throws IOException
    */
   @Test
-  @Ignore("TODO: Re-enable this test, it's failing for Netty only. See MULE-8502")
   public void persistentStreamingTransformerCheckHeader() throws IOException {
     assertThat(performRequest(persistentStreamingTransformerPort.getNumber(), getHttpVersion(), true), is(CLOSE));
   }
