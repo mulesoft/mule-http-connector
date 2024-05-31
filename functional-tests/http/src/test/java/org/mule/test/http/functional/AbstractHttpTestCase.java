dd1148a0662a898068e21c18e435f70b3e73b863
Ezequiel Werner <eze210@gmail.com>
2024-05-30 23:09:20 -0300

    msg

--- functional-tests/http/src/test/java/org/mule/test/http/functional/AbstractHttpTestCase.java
+++ functional-tests/http/src/test/java/org/mule/test/http/functional/AbstractHttpTestCase.java
@@ -56,6 +56,7 @@ public abstract class AbstractHttpTestCase extends MuleArtifactFunctionalTestCas
   protected static final String NETTY_SSL_ERROR_RESPONSE = "Received fatal alert: handshake_failure";
   protected static final String APPROPRIATE_PROTOCOL_ERROR =
       "No appropriate protocol (protocol is disabled or cipher suites are inappropriate)";
+  protected static final String NETTY_APPROPRIATE_PROTOCOL_ERROR = "Received fatal alert: protocol_version";
 
   @Rule
   public TestHttpClient httpClient = new TestHttpClient.Builder(getService(HttpService.class)).build();
@@ -86,6 +87,7 @@ public abstract class AbstractHttpTestCase extends MuleArtifactFunctionalTestCas
                     containsString(J11_SSL_ERROR_RESPONSE),
                     containsString(J17_SSL_ERROR_RESPONSE),
                     containsString(NETTY_SSL_ERROR_RESPONSE),
-                    containsString(APPROPRIATE_PROTOCOL_ERROR)));
+                    containsString(APPROPRIATE_PROTOCOL_ERROR),
+                    containsString(NETTY_APPROPRIATE_PROTOCOL_ERROR)));
   }
 }
