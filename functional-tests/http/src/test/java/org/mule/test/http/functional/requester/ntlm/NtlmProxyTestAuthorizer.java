7727cb23b5cc46d794727e8cf1f57179255e7777
elrodro83 <rmerino@salesforce.com>
2025-01-29 12:00:08 -0300

    W-17369306: Update Jetty/Wiremock to latest in the whole Uber

--- functional-tests/http/src/test/java/org/mule/test/http/functional/requester/ntlm/NtlmProxyTestAuthorizer.java
+++ functional-tests/http/src/test/java/org/mule/test/http/functional/requester/ntlm/NtlmProxyTestAuthorizer.java
@@ -6,13 +6,6 @@
  */
 package org.mule.test.http.functional.requester.ntlm;
 
-/*
- * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
- * The software in this package is published under the terms of the CPAL v1.0
- * license, a copy of which has been included with this distribution in the
- * LICENSE.txt file.
- */
-
 import static javax.servlet.http.HttpServletResponse.SC_OK;
 import static javax.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
 
@@ -21,8 +14,8 @@ import org.mule.test.http.functional.requester.TestAuthorizer;
 
 import java.io.IOException;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public class NtlmProxyTestAuthorizer implements TestAuthorizer {
 
