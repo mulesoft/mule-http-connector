b0ee8e88b9e7bbfbc334bcdb4a1f9637b5315b17
Ezequiel Werner <eze210@gmail.com>
2025-03-11 13:05:51 -0300

    missing

--- mule-http-connector/src/main/java/org/mule/extension/http/internal/request/client/HttpExtensionClient.java
+++ mule-http-connector/src/main/java/org/mule/extension/http/internal/request/client/HttpExtensionClient.java
@@ -6,6 +6,7 @@
  */
 package org.mule.extension.http.internal.request.client;
 
+import static org.mule.extension.http.internal.request.UriUtils.resolveUri;
 import static org.mule.runtime.core.api.lifecycle.LifecycleUtils.startIfNeeded;
 import static org.mule.runtime.core.api.lifecycle.LifecycleUtils.stopIfNeeded;
 
@@ -16,7 +17,6 @@ import org.mule.extension.http.internal.request.ShareableHttpClient;
 import org.mule.runtime.api.exception.MuleException;
 import org.mule.runtime.api.lifecycle.Startable;
 import org.mule.runtime.api.lifecycle.Stoppable;
-import org.mule.runtime.http.api.client.HttpClient;
 import org.mule.runtime.http.api.client.auth.HttpAuthentication;
 import org.mule.runtime.http.api.domain.message.request.HttpRequest;
 import org.mule.runtime.http.api.domain.message.response.HttpResponse;
@@ -24,7 +24,9 @@ import org.mule.sdk.api.http.sse.ClientWithSse;
 import org.mule.sdk.api.http.sse.ServerSentEventSource;
 import org.mule.sdk.api.http.sse.SseRetryConfig;
 
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Composition of a {@link ShareableHttpClient} with URI and authentication parameters that allow falling back to connection
@@ -37,6 +39,7 @@ public class HttpExtensionClient implements Startable, Stoppable, ClientWithSse 
   private final HttpRequestAuthentication authentication;
   private final ShareableHttpClient httpClient;
   private final UriParameters uriParameters;
+  private final Map<String, ServerSentEventSource> sseSourcesByPath = new ConcurrentHashMap<>();
 
   public HttpExtensionClient(ShareableHttpClient httpClient, UriParameters uriParameters,
                              HttpRequestAuthentication authentication) {
@@ -80,7 +83,10 @@ public class HttpExtensionClient implements Startable, Stoppable, ClientWithSse 
   }
 
   @Override
-  public ServerSentEventSource sseSource(String url, SseRetryConfig retryConfig) {
-    return httpClient.sseSource(url, retryConfig);
+  public ServerSentEventSource sseSource(String path, SseRetryConfig retryConfig) {
+    return sseSourcesByPath.computeIfAbsent(path, p -> {
+      String uri = resolveUri(uriParameters.getScheme(), uriParameters.getHost().trim(), uriParameters.getPort(), path);
+      return httpClient.sseSource(uri, retryConfig);
+    });
   }
 }
