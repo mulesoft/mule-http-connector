2b96e02d01e83a8c1fca6b511aac917c873e7a46
aronee2008 <aronee.dasgupta@salesforce.com>
2025-03-21 12:37:38 -0700

    fix build yaml

--- mule-http-mock-extension/src/main/java/org/mule/test/extensions/http/mock/internal/server/DelegateToFlowTransformer.java
+++ mule-http-mock-extension/src/main/java/org/mule/test/extensions/http/mock/internal/server/DelegateToFlowTransformer.java
@@ -18,16 +18,17 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
+import com.github.tomakehurst.wiremock.common.FileSource;
 import com.github.tomakehurst.wiremock.extension.Parameters;
-import com.github.tomakehurst.wiremock.extension.ResponseDefinitionTransformerV2;
+import com.github.tomakehurst.wiremock.extension.ResponseDefinitionTransformer;
+import com.github.tomakehurst.wiremock.http.Request;
 import com.github.tomakehurst.wiremock.http.ResponseDefinition;
-import com.github.tomakehurst.wiremock.stubbing.ServeEvent;
 import org.slf4j.Logger;
 
 /**
  * WireMock transformer used to delegate the request received in the server-endpoint source to the rest of the flow.
  */
-public class DelegateToFlowTransformer implements ResponseDefinitionTransformerV2 {
+public class DelegateToFlowTransformer extends ResponseDefinitionTransformer {
 
   public static final String TRANSFORMER_NAME = "delegate-to-flow-transformer";
   public static final String SOURCE_CALLBACK_WIREMOCK_PARAMETER = "source-callback";
@@ -36,31 +37,47 @@ public class DelegateToFlowTransformer implements ResponseDefinitionTransformerV
   private static final Logger LOGGER = getLogger(DelegateToFlowTransformer.class);
 
   @Override
-  public ResponseDefinition transform(ServeEvent serveEvent) {
+  public ResponseDefinition transform(Request request, ResponseDefinition responseDefinition, FileSource files,
+                                      Parameters parameters) {
     LOGGER.debug("transform() called");
-    Parameters parameters = serveEvent.getTransformerParameters();
+
     SourceCallback<InputStream, HTTPMockRequestAttributes> sourceCallback =
         (SourceCallback) parameters.get(SOURCE_CALLBACK_WIREMOCK_PARAMETER);
+
+    if (sourceCallback == null) {
+      LOGGER.error("SourceCallback is missing in the parameters");
+      return ResponseDefinitionBuilder
+          .like(responseDefinition)
+          .but()
+          .withStatus(500)
+          .withBody("Source callback not found")
+          .build();
+    }
+
     SourceCallbackContext sourceContext = sourceCallback.createContext();
 
     CompletableFuture<ResponseDefinition> responseFuture = new CompletableFuture<>();
     sourceContext.addVariable(RESPONSE_FUTURE_PARAMETER, responseFuture);
 
     sourceCallback.handle(Result.<InputStream, HTTPMockRequestAttributes>builder()
-        .output(getBodyAsInputStream(serveEvent))
-        .attributes(getRequestAttributes(serveEvent))
+        .output(getBodyAsInputStream(request))
+        .attributes(getRequestAttributes(request))
         .build(), sourceContext);
 
     try {
-      return responseFuture.get();
+      ResponseDefinition flowResponse = responseFuture.get();
+      LOGGER.debug("Flow completed successfully with response: {}", flowResponse);
+      return flowResponse;
     } catch (InterruptedException e) {
-      LOGGER.error("Error occurred while waiting for flow to finish", e);
+      LOGGER.error("Interrupted while waiting for the flow to complete", e);
       Thread.currentThread().interrupt();
     } catch (ExecutionException e) {
-      LOGGER.error("Error occurred while waiting for flow to finish", e);
+      LOGGER.error("Execution exception while waiting for the flow to complete", e);
     }
 
-    return new ResponseDefinitionBuilder()
+    return ResponseDefinitionBuilder
+        .like(responseDefinition)
+        .but()
         .withStatus(500)
         .withBody("Error while waiting for flow to finish")
         .build();
@@ -76,16 +93,17 @@ public class DelegateToFlowTransformer implements ResponseDefinitionTransformerV
     return false;
   }
 
-  private static ByteArrayInputStream getBodyAsInputStream(ServeEvent serveEvent) {
-    byte[] bodyBytes = serveEvent.getRequest().getBody();
+  private static ByteArrayInputStream getBodyAsInputStream(Request request) {
+    byte[] bodyBytes = request.getBody();
     if (LOGGER.isInfoEnabled()) {
       LOGGER.info("Received body as String: {}", new String(bodyBytes));
     }
     return new ByteArrayInputStream(bodyBytes);
   }
 
-  // TODO: Implement!
-  private static HTTPMockRequestAttributes getRequestAttributes(ServeEvent serveEvent) {
-    return new HTTPMockRequestAttributes();
+  private static HTTPMockRequestAttributes getRequestAttributes(Request request) {
+    HTTPMockRequestAttributes attributes = new HTTPMockRequestAttributes();
+    // TODO: Extract and populate attributes from the request if necessary
+    return attributes;
   }
 }
